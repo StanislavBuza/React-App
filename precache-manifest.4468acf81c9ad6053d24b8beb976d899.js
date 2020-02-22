73a81fc03734212cbb9a554711d0de1b84bb1574
Stanislav Buza <stanislavbuza@yahoo.com>
2020-02-22 11:48:52 -0600

    Updates

---
+++ precache-manifest.4468acf81c9ad6053d24b8beb976d899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8668cffcadfc8db916d375cf712813b2",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "6b848d06c71ebf308aa6",
+    "url": "/React-App/static/css/main.b403b0d2.chunk.css"
+  },
+  {
+    "revision": "671cf3a43996f2a822c6",
+    "url": "/React-App/static/js/2.36d51abf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-App/static/js/2.36d51abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b848d06c71ebf308aa6",
+    "url": "/React-App/static/js/main.893e8e91.chunk.js"
+  },
+  {
+    "revision": "28106e2875ebfcf89828",
+    "url": "/React-App/static/js/runtime-main.d7c022de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-App/static/media/logo.5d5d9eef.svg"
+  }
+]);
